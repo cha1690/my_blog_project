9a09079be66d9f9b9e40bfe3309b4e3f42e1fdcd
Rucha Deshpande <rucha.d1690@gmail.com>
2020-03-03 13:04:08 +0530

    about page changes

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from blog import views
 
 
+
 urlpatterns = [
 	url(r'^$', views.PostListView.as_view(), name='post_list'),
 	url(r'^about/$', views.AboutView.as_view(), name='about'),
--- blog/views.py
+++ blog/views.py
@@ -17,7 +17,7 @@ class PostListView(ListView):
 	model = Post
 
 	def get_queryset(self):
-		return Post.objects.filter(published_date__lte= timezone.now()).order_by('-published_date')
+		return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
 class PostDetailView(DetailView):
 	model = Post
